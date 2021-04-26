c917c6459dd36d1f1c76f27660da00cec5362664
Joshua Anderson <joshuatanderson1@gmail.com>
2021-04-26 13:00:56 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import web3 from './web3';
+
+
+// this first application assumes that metamask is installed.
+// web 3 gotchas
+// metamask AUTOMATICALLY injects a web 3 instance into any webpage.  We want a DIFFERENT one, so 
+// we can control it, and use the API we want, instead of an older one.
+// Howeer, we do want to rip out the PROVIDER of the old version, since it has the keys
+// of the user, and splice it into the new version.
 
 function App() {
+  // useEffect(async () => {
+  //   const x = await web3.eth.getAccounts()
+  //   console.log(x)
+  // }, [])
   return (
     <div className="App">
       <header className="App-header">
